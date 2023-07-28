3bfae8f2d8fcfc95da76ebb150004f019905433a
AkamshuSunday <akamshu.sunday6@gmail.com>
2023-07-28 06:57:13 -0700

    argc_argv tasks

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,6 @@
  * description - Write a C program that prints exactly and that piece of
  * art is useful\"
  *  - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
- *
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -12,6 +11,5 @@ int main(void)
         write(2,
               "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
               59);
-
         return (1);
 }
