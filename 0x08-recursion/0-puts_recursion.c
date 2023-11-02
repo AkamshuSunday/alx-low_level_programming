541a9913e8b436b28f69b295d6093fc188954025
“AkamshuSunday” <akamshu.sunday6@gmail.com>
2023-11-02 00:51:07 -0700

    0x08-recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void _puts_recursion(char *m) {
+    if (*m == '\0') {
+        putchar('\n'); //it will print a new line when the end of the string is reached
+        return;
+    }
+    putchar(*m); // the line will print the current character
+    _puts_recursion(m + 1); // Recursive with the next character in the string
+}
+
