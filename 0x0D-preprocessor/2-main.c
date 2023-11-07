683c4fd7c40d4b9839bd9493871757119f6eae33
“AkamshuSunday” <akamshu.sunday6@gmail.com>
2023-11-07 04:19:47 -0800

    Preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -4,12 +4,7 @@
  * main - prints the name of the file
  *
  * Return: Always 0 (Success)
- *
- *  The main function is the entry point of the program.
- *  In this case, it prints the name of the file using __FILE__.
- *
- *  The function returns 0 to indicate successful execution.
-*/
+ */
 int ain(void)
 {
 	printf("%s\n", __FILE__);
