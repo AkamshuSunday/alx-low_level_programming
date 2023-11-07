ab0d1c6039a66d43086d890ff04c6ad749491a91
“AkamshuSunday” <akamshu.sunday6@gmail.com>
2023-11-07 04:04:34 -0800

    Preprocessor

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,21 +1,16 @@
 #ifndef SUM_H
 #define SUM_H
 
-/* 
-   The following code uses an include guard to prevent multiple inclusions
-   of this header file. If SUM_H has not been defined yet, it gets defined here.
+/* The following code uses an include guard to prevent multiple inclusions
+   of this header file. 
+   If SUM_H has not been defined yet, it gets defined here.
+   The macro SUM is defined as a function-like macro. It takes two arguments,
+   'x' and 'y', and returns the sum of these two values. The macro performs 
+   addition using parentheses to ensure correct evaluation.
+   This marks the end of the header
 */
 
 #define SUM(x, y) ((x) + (y))
 
-/* 
-   The macro SUM is defined as a function-like macro. It takes two arguments,
-   'x' and 'y', and returns the sum of these two values. The macro performs 
-   addition using parentheses to ensure correct evaluation.
-*/
-
 #endif
-
-/* 
-   This marks the end of the header
 
