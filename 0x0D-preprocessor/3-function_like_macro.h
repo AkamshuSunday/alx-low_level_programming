d9cf073f2cc6f8d459e0f1ed7fd14b90124ed12a
“AkamshuSunday” <akamshu.sunday6@gmail.com>
2023-11-07 04:09:03 -0800

    Preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -5,20 +5,14 @@
    The following code uses an include guard to prevent multiple inclusions
    of this header file. If FUNCTION_LIKE_MACRO_H has not been defined yet,
    it gets defined here.
+   The macro ABS is defined as a function-like macro. It takes a single 
+   argument 'x' and returns the absolute value of 'x'. The macro performs 
+   a conditional expression to determine the absolute value. 
+   This marks the end of the header file. The #endif corresponds to the #ifndef 
+   at the beginning, effectively closing the include guard.
 */
 
 #define ABS(x) ((x) < (0) ? -(x) : (x))
 
-/* 
-   The macro ABS is defined as a function-like macro. It takes a single 
-   argument 'x' and returns the absolute value of 'x'. The macro performs 
-   a conditional expression to determine the absolute value.
-*/
-
 #endif
-
-/* 
-   This marks the end of the header file. The #endif corresponds to the #ifndef 
-   at the beginning, effectively closing the include guard.
-*/
 
