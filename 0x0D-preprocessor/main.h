74196289f833d0cb1a849b1fab70b13c02870c8d
“AkamshuSunday” <akamshu.sunday6@gmail.com>
2023-11-07 04:34:43 -0800

    Preprocessor

---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Task 0 */
+#define SIZE 1024
+
+/* Task 1 */
+#define PI 3.14159265359
+
+/* Task 3 */
+void print_filename(void);
+
+/* Task 4 */
+#define ABS(x) (((x) < 0) ? -(x) : (x))
+#define SUM(x, y) ((x) + (y))
+
+#endif /* MAIN_H */
