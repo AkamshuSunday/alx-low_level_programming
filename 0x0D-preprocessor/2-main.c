ee8bfde0b3071c50bae2f6ded14d0b2db1ce562d
“AkamshuSunday” <akamshu.sunday6@gmail.com>
2023-11-08 09:10:59 -0800

    Preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file
+ * main - Entry point of the program
+ *
+ * Description: This function prints the name of the file and returns 0
  *
  * Return: Always 0 (Success)
  */
-int ain(void)
+int main(void)
 {
+	/* Print the name of the file followed by a newline */
 	printf("%s\n", __FILE__);
+
+	/* Return 0 to indicate successful execution */
 	return (0);
 }
 
