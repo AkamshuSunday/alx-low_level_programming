3b0736402ed084c409ccda5053291b468a50b510
AkamshuSunday <akamshu.sunday6@gmail.com>
2023-12-22 08:07:00 -0800

    0x0A-argc_argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,12 +7,12 @@
  * Return: (0) if successful
  */
 int main(int argc, char *argv[])
-{       
-        if (argc != 3)
-        {
-                printf("Error\n");
-                return (1);
-        }
-        printf("%d\n", atoi (argv[1]) * atoi (argv[2]));
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
-        }
+}
