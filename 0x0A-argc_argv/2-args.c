e18bdce6a4dee06bd8dd3e96aef4da5110f83fcc
AkamshuSunday <akamshu.sunday6@gmail.com>
2023-12-22 07:18:23 -0800

    Write a program that multiplies two numbers

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - will print all arguments it receives
+ * @argc: number of comman line argument
+ * @argv: array that contains the program comman line argument
+ * Return: (0) if successful
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv [i]);
+	}
+	return (0);
+}
